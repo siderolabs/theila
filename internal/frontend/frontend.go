2eb7fb1487617d8aaadc1f97911f92b74ce340eb
Artem Chernyshev <artem.0xD2@gmail.com>
2020-09-17 16:26:30 +0300

    feat: create hello world Vue app using Kres
    Makefiles, Dockerfiles, various template generation is handled purely by
    Kres.
    
    Signed-off-by: Artem Chernyshev <artem.0xD2@gmail.com>

---
+++ internal/frontend/frontend.go
@@ -0,0 +1,15 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// THIS FILE WAS AUTOMATICALLY GENERATED, PLEASE DO NOT EDIT.
+//
+// Generated on 2021-03-30T13:16:09Z by kres 9f398f8-dirty.
+
+package frontend
+
+import "embed"
+
+// Dist is an embedded JS frontend release folder.
+//go:embed dist
+var Dist embed.FS
