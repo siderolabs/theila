b273caea27116b6f0e9863b9b4bedf9bf2c8cbfb
Artem Chernyshev <artem.0xD2@gmail.com>
2021-06-22 17:44:43 +0300

    feat: add ability to pick sort order on the servers page
    Sort options are configurable by the input rules, so it's easy to add
    that on the other pages as soon as we decide on what options we want to
    have there.
    
    Signed-off-by: Artem Chernyshev <artem.0xD2@gmail.com>

--- frontend/src/components/Watch.vue
+++ frontend/src/components/Watch.vue
@@ -13,6 +13,17 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
     </t-alert>
     <t-alert v-else-if="items.length == 0" type="info" title="No Records">No entries of the requested resource type are found on the server.</t-alert>
     <stacked-list v-else :items="items" :idFn="(item) => resourceWatch.id(item)" :showCount="showCount" :itemName="itemName" :search="search" :filterFn="filter" :categories="categories">
+      <template v-slot:menu v-if="sortOptions">
+        <t-dropdown :title="'Sorted by: ' + sortName">
+          <menu-item v-for="option in sortOptions" :key="option.name" v-slot="{ active }">
+            <a
+              v-on:click="() => { sortFn = option.function; sortName = option.name }"
+              :class="{ active }"
+              >{{ option.name }}</a
+            >
+          </menu-item>
+        </t-dropdown>
+      </template>
       <template v-slot:header v-if="$slots.header">
         <slot name="header"></slot>
       </template>
@@ -26,15 +37,20 @@ file, You can obtain one at http://mozilla.org/MPL/2.0/.
 <script lang="ts">
 import { ref, toRefs, computed } from "vue";
 import { context as ctx } from "../context";
+import { MenuItem } from '@headlessui/vue';
+import { defaultCompareFunc } from "../api/watch";
 import Watch from "../api/watch";
 import TSpinner from './TSpinner.vue';
 import TAlert from './TAlert.vue';
+import TDropdown from './TDropdown.vue';
 import StackedList from './StackedList.vue';
 
 export default {
   components: {
     TSpinner,
     TAlert,
+    TDropdown,
+    MenuItem,
     StackedList,
   },
 
@@ -46,6 +62,7 @@ export default {
     categories: Array,
     search: String,
     filterFn: Function,
+    sortOptions: Object,
     compareFn: Function,
     kubernetes: Boolean,
     talos: Boolean,
@@ -53,11 +70,20 @@ export default {
   },
 
   setup(props, context) {
-    const { filterFn } = toRefs(props);
+    const { filterFn, sortOptions } = toRefs(props);
     const resourceWatch = props.watch ? props.watch : new Watch(
       ctx.api,
       ref([]),
     );
+
+    const sortName = ref("Default");
+
+    if(sortOptions && sortOptions.value) {
+      sortOptions.value.splice(0, 0, {
+        name: "Default",
+        function: defaultCompareFunc(resourceWatch),
+      });
+    }
 
     if(!props.watch) {
       resourceWatch.setup(props, context);
@@ -68,6 +94,8 @@ export default {
       err: resourceWatch.err,
       loading: resourceWatch.loading,
       running: resourceWatch.running,
+      sortFn: resourceWatch.sort,
+      sortName,
       resourceWatch,
       filter: computed(() => {
         return filterFn && filterFn.value ? filterFn.value : (item, filter) => resourceWatch.id(item).includes(filter);
