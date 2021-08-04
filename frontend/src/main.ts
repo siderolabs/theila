672c517d6444da67bd9db7a9ddb3ece3a62b37e1
Artem Chernyshev <artem.0xD2@gmail.com>
2021-08-04 18:16:56 +0300

    fix: use polyfill to fix streaming APIs on Firefox
    Fixes: https://github.com/talos-systems/theila/issues/73
    
    Signed-off-by: Artem Chernyshev <artem.0xD2@gmail.com>

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,6 +2,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import "@stardazed/streams-polyfill";
 import { createApp } from 'vue';
 import './index.css';
 import App from './App.vue'
